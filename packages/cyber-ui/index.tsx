a025860af7a6863cd094d20a5f3610a270605e85
patrickasafe <patrickasafe@gmail.com>
2022-11-11 18:04:43 -0300

    ADD Refrigerator SVG component

--- packages/cyber-ui/index.tsx
+++ packages/cyber-ui/index.tsx
@@ -2,3 +2,7 @@ import * as React from "react";
 export * from "./Button";
 export * from "./components/Table";
 export * from "./styles/stitches.config";
+export * from "./components/images/Refrigerator";
+export * from "./components/HeaderTitle";
+export * from "./components/LayoutContainer";
+export * from "./components/HeaderContainer";
