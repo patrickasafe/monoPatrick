8db992016aa1a8b70354ead7efb495f9078887ef
patrickasafe <patrickasafe@gmail.com>
2022-12-20 18:50:05 -0300

    EDIT notifications-service app

---
+++ apps/notifications-service/src/application/entities/notification.test.ts
@@ -0,0 +1,23 @@
+import { Content } from './content';
+import { Notification } from './notification';
+
+describe('Notification content', () => {
+  it('should be able to create a notification', () => {
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizade'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+      crea
+    });
+
+    expect(content).toBeTruthy();
+  });
+
+  it('should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('Você')).toThrow();
+  });
+
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
+});
