13088da512d78c869cc3873f1a83e11a6721575a
patrickasafe <patrickasafe@gmail.com>
2023-02-11 15:17:30 -0300

    EDIT axios baseUrl

--- apps/wasteless/lib/axiosInstance/constants.ts
+++ apps/wasteless/lib/axiosInstance/constants.ts
@@ -2,5 +2,5 @@ const testBaseURL = "http://localhost:3000/api/";
 
 const devBaseURL = "https://mono-patrick-wasteless.vercel.app/api/";
 
-export const baseUrl: string =
-  process.env.NODE_ENV === "test" ? testBaseURL : devBaseURL;
+export const baseUrl: string = "https://mono-patrick-wasteless.vercel.app/api/"
+  // process.env.NODE_ENV === "test" ? testBaseURL : devBaseURL;
