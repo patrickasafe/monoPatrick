3844bcf16a270607113fb7adffc03c2c84db9c7e
patrickasafe <patrickasafe@gmail.com>
2023-02-11 14:20:20 -0300

    EDIT update prisma and next-auth

--- apps/wasteless/lib/axiosInstance/constants.ts
+++ apps/wasteless/lib/axiosInstance/constants.ts
@@ -1,6 +1,6 @@
 const testBaseURL = "http://localhost:3000/api/";
 
-const devBaseURL = "http://mono-patrick-wasteless.vercel.app/api/";
+const devBaseURL = "https://mono-patrick-wasteless.vercel.app/api/";
 
 export const baseUrl: string =
   process.env.NODE_ENV === "test" ? testBaseURL : devBaseURL;
