f26adada7db7dbdf08800fdd96a46f385da4f694
patrickasafe <patrickasafe@gmail.com>
2023-02-15 20:34:26 -0300

    EDIT package.json 7

--- apps/wasteless/lib/axiosInstance/index.ts
+++ apps/wasteless/lib/axiosInstance/index.ts
@@ -11,5 +11,5 @@ import { baseUrl } from "./constants";
 //   return { Authorization: `Bearer ${user.token}` };
 // }
 
-const config: AxiosRequestConfig = {};
+const config: AxiosRequestConfig = { baseURL: baseUrl };
 export const axiosInstance = axios.create(config);
